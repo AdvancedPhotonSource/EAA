3f10509308466dd8a0c637962866916b1a4de20d
Ming Du <ming.du101@gmail.com>
2025-05-06 20:26:52 -0500

    FEAT: allow selecting group chat speaker selection method

--- src/eaa/task_managers/imaging/setup_param_search.py
+++ src/eaa/task_managers/imaging/setup_param_search.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from textwrap import dedent
 
 import autogen
@@ -21,6 +22,7 @@ class SetupParameterSearchTaskManager(BaseTaskManager):
         model_name: str = "gpt-4o", 
         model_base_url: str = None,
         tools: list[BaseTool] = [],
+        speaker_selection_method: Literal["round_robin", "random", "auto"] = "auto",
         *args, **kwargs
     ) -> None:
         """An agent that searches for the best setup parameters
@@ -32,14 +34,25 @@ class SetupParameterSearchTaskManager(BaseTaskManager):
             The name of the model to use.
         model_base_url : str, optional
             The base URL of the model. This is only needed for self-hosted models.
+        tools : list[BaseTool], optional
+            A list of tools given to the agent.
+        speaker_selection_method : Literal["round_robin", "random", "auto"], optional
+            The method to select the next speaker in the group chat.
+            - "round_robin": select the next speaker in a round-robin fashion.
+            - "random": select the next speaker randomly.
+            - "auto": let the LLM decide the next speaker. Some models might have issues
+              with suggesting the speaker in the right format when used as the group chat
+              manager. In that case, use "round_robin" or "random" instead.
         """
+        self.speaker_selection_method = speaker_selection_method
+        
         super().__init__(
             model_name=model_name, 
             model_base_url=model_base_url,
             tools=tools, 
             *args, **kwargs
         )
-                
+
     def build_agents(self, *args, **kwargs) -> None:
         """Build the assistant(s)."""
         
@@ -92,6 +105,7 @@ class SetupParameterSearchTaskManager(BaseTaskManager):
             allow_repeat_speaker=False,
             messages=[],
             max_round=999,
+            speaker_selection_method=self.speaker_selection_method,
         )
         
         self.agents.group_chat_manager = autogen.GroupChatManager(
