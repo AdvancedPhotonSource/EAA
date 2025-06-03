a9ed3e5345e6aa1876df5a59f9ee7271103a6cb5
Ming Du <ming.du101@gmail.com>
2025-06-03 15:48:19 -0500

    FEAT: agent with OpenAI Python API

---
+++ src/eaa/agents/openai.py
@@ -0,0 +1,328 @@
+import inspect
+import typing
+import os
+import json
+from typing import get_type_hints, Callable, Dict, Any, Literal, List
+
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessage
+
+
+class ToolManager:
+    
+    def __init__(self):
+        self.tools: List[Dict[str, Any]] = []
+    
+    def get_all_schema(self) -> Dict[str, Any]:
+        """Get the schema for the tool.
+        """
+        return [generate_openai_tool_schema(tool["function"]) for tool in self.tools]
+    
+    def add_tool(self, name: str, tool_function: Callable) -> None:
+        """Add a tool to the tool manager.
+        
+        Parameters
+        ----------
+        name : str
+            The name of the tool.
+        tool_function : Callable
+            The function to be used as a tool. The function should be type-annotated,
+            and have a docstring to be used as the description of the tool.
+        """
+        self.tools.append(
+            {
+                "name": name,
+                "function": tool_function,
+                "schema": generate_openai_tool_schema(tool_function)
+            }
+        )
+        
+    def execute_tool(
+        self,
+        tool_name: str,
+        tool_kwargs: Dict[str, Any]
+    ) -> Any:
+        """Execute a tool.
+        
+        Parameters
+        ----------
+        tool_name : str
+            The name of the tool.
+        tool_kwargs : Dict[str, Any]
+            The arguments to be passed to the tool.
+        """
+        for tool in self.tools:
+            if tool["name"] == tool_name:
+                return tool["function"](**tool_kwargs)
+        raise ValueError(f"Tool {tool_name} not found.")
+
+
+class OpenAIAgent:
+    
+    def __init__(
+        self,
+        llm_config: dict,
+        system_message: str = None,
+    ) -> None:
+        """An agent that uses OpenAI-compatible API to generate responses.
+
+        Parameters
+        ----------
+        llm_config : dict
+            Configuration for the OpenAI-compatible API. It should be a dictionary with
+            the following keys:
+            - `model`: The name of the model.
+            - `api_key`: The API key for the OpenAI-compatible API.
+            - `base_url`: The base URL for the OpenAI-compatible API.
+        system_message : str, optional
+            The system message for the OpenAI-compatible API.
+        """
+        self.model = llm_config.get("model")
+        self.api_key = llm_config.get("api_key", "")
+        self.base_url = llm_config.get("base_url", "https://api.openai.com/v1")
+        
+        self.messages = [
+            {"role": "system", "content": system_message}
+        ]
+        self.tool_manager = ToolManager()
+        
+        self.client = OpenAI(
+            api_key=self.api_key,
+            base_url=self.base_url,
+        )
+        
+    def register_tools(self, tools: Dict[str, Callable]) -> None:
+        """Register tools with the OpenAI-compatible API.
+        
+        Parameters
+        ----------
+        tools : Dict[str, Callable]
+            A dictionary of tool names and their corresponding callable functions.
+        """
+        if not isinstance(tools, Dict):
+            raise ValueError(
+                "tools must be a dictionary of tool names and their corresponding callable functions."
+            )
+        
+        for tool_name, tool_function in tools.items():
+            self.tool_manager.add_tool(tool_name, tool_function)
+        
+    def receive(
+        self,
+        message: str | Dict[str, Any], 
+        return_full_response: bool = True,
+        with_history: bool = True
+    ) -> str | Dict[str, Any]:
+        """Receive a message from the user and generate a response.
+        
+        Parameters
+        ----------
+        message : str | Dict[str, Any]
+            The message to be sent to the AI. It should be 
+        return_full_response : bool, optional
+            If True, this function returns a dictionary containing the full response
+            from the agent. Otherwise, only the content of the response is returned
+            as a string.
+        with_history : bool, optional
+            If True, the history of the conversation is included in the message sent
+            to the agent so that it has the memory of the conversation.
+        
+        Returns
+        -------
+        str | Dict[str, Any]
+            The response from the agent. If `return_full_response` is True, the
+            response is a dictionary containing the full response from the agent.
+            Otherwise, the response is a string containing the content of the response.
+        """
+        if isinstance(message, str):
+            message = generate_openai_message(message)
+        response = self.send_message_and_get_response(message, with_history=with_history)
+        
+        # Add incoming message to history
+        self.messages.append(message)
+        
+        # Add response to history
+        self.messages.append(response)
+        
+        if return_full_response:
+            return dict(response)
+        else:
+            return response.choices[0].message.content
+    
+    def send_message_and_get_response(
+        self,
+        message: Dict[str, Any],
+        with_history: bool = True
+    ) -> Dict[str, Any]:
+        """Send a message to the agent and get the response.
+        
+        Parameters
+        ----------
+        message : Dict[str, Any]
+            The message to be sent to the agent.
+        with_history : bool, optional
+            If True, the history of the conversation is included in the message sent
+            to the agent so that it has the memory of the conversation.
+        
+        Returns
+        -------
+        Dict[str, Any]
+            The response from the agent.
+        """
+        if with_history:
+            messages = self.messages + [message]
+        else:
+            messages = [message]
+            
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            tools=self.tool_manager.get_all_schema(),
+            tool_choice="auto",
+        )
+        return response.choices[0].message.to_dict()
+    
+    def handle_tool_call(self, message: Dict[str, Any]) -> Dict[str, Any] | None:
+        """Handle the tool call in the response of the agent.
+        If tool call exists, the tool will be executed and a tool
+        response will be returned. Otherwise, this function returns
+        None.
+        """
+        if not has_tool_call(message):
+            return None
+        tool_call_info = get_tool_call_info(message)
+        tool_call_id = tool_call_info["id"]
+        tool_name = tool_call_info["function"]["name"]
+        tool_call_kwargs = json.loads(tool_call_info["function"]["arguments"])
+        result = self.tool_manager.execute_tool(tool_name, tool_call_kwargs)
+        response = generate_openai_message(
+            content=str(result),
+            role="tool",
+            tool_call_id=tool_call_id
+        )
+        return response
+    
+def to_dict(message: ChatCompletionMessage | Dict[str, Any]) -> dict:
+    """Convert a ChatCompletionMessage to a dictionary.
+    """
+    if isinstance(message, ChatCompletionMessage):
+        return message.to_dict()
+    else:
+        return message
+
+
+def generate_openai_message(
+    content: str,
+    role: Literal["user", "system", "tool"] = "user",
+    tool_call_id: str = None
+) -> Dict[str, Any]:
+    """Generate a dictionary containing the message to be sent
+    to the agent.
+    
+    Parameters
+    ----------
+    content : str
+        The content of the message.
+    role : Literal["user", "system", "tool"], optional
+        The role of the sender.
+    """
+    if role == "user":
+        message = {
+            "role": "user",
+            "content": content
+        }
+    elif role == "system":
+        message = {
+            "role": "system",
+            "content": content
+        }
+    elif role == "tool":
+        message = {
+            "role": "tool",
+            "content": content,
+            "tool_call_id": tool_call_id
+        }
+    return message
+
+
+def has_tool_call(message: dict | ChatCompletionMessage) -> bool:
+    """Check if the message has a tool call.
+    """
+    message = to_dict(message)
+    if "tool_calls" in message.keys():
+        return True
+    else:
+        return False
+
+
+def get_tool_call_info(message: dict | ChatCompletionMessage, index=0) -> str:
+    """Get the tool call ID from the message.
+    """
+    message = to_dict(message)
+    return message["tool_calls"][index]
+
+
+def generate_openai_tool_schema(func: Callable) -> Dict[str, Any]:
+    """
+    Generates an OpenAI-compatible tool schema from a Python function
+    with type annotations and a docstring.
+    
+    Parameters
+    ----------
+    func : Callable
+        The function to generate the tool schema from.
+        
+    Returns
+    -------
+    dict
+        The OpenAI-compatible tool schema.
+    """
+    sig = inspect.signature(func)
+    type_hints = get_type_hints(func)
+    doc = inspect.getdoc(func) or ""
+    
+    # JSON schema type mapping
+    python_type_to_json = {
+        str: "string",
+        int: "integer",
+        float: "number",
+        bool: "boolean",
+        list: "array",
+        tuple: "array",
+        dict: "object"
+    }
+
+    def resolve_json_type(py_type):
+        origin = typing.get_origin(py_type)
+        args = typing.get_args(py_type)
+        if origin is list or origin is typing.List:
+            return {
+                "type": "array",
+                "items": {"type": python_type_to_json.get(args[0], "string")}
+            }
+        return {"type": python_type_to_json.get(py_type, "string")}
+
+    properties = {}
+    required = []
+
+    for name, param in sig.parameters.items():
+        if name not in type_hints:
+            continue
+        json_type = resolve_json_type(type_hints[name])
+        description = f"{name} parameter"
+        properties[name] = {**json_type, "description": description}
+        if param.default == inspect.Parameter.empty:
+            required.append(name)
+
+    return {
+        "type": "function",
+        "function": {
+            "name": func.__name__,
+            "description": doc,
+            "parameters": {
+                "type": "object",
+                "properties": properties,
+                "required": required
+            }
+        }
+    }
---
+++ tests/test_oai_api.py
@@ -0,0 +1,65 @@
+
+import argparse
+import logging
+import os
+
+import pytest
+import numpy as np
+
+from eaa.agents.openai import OpenAIAgent
+
+import test_utils as tutils
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class TestOpenAIAPI(tutils.BaseTester):
+    
+    @pytest.mark.local
+    def test_openai_api(self):
+        
+        def list_sum(numbers: list[float]) -> float:
+            """
+            Sum all the numbers in the list.
+            
+            Parameters
+            ----------
+            numbers : list[float]
+                The list of numbers to sum.
+                
+            Returns
+            -------
+            float
+                The sum of the numbers in the list.
+            """
+            return np.sum(numbers)
+        
+        
+        agent = OpenAIAgent(
+            llm_config={
+                "model": "openai/gpt-4o-2024-11-20",
+                "api_key": os.getenv("OPENROUTER_API_KEY"),
+                "base_url": "https://openrouter.ai/api/v1",
+            },
+            system_message="You are a helpful assistant."
+        )
+        
+        agent.register_tools(
+            {"list_sum": list_sum}
+        )
+        
+        response = agent.receive("Can you sum these numbers: 2, 4, 6, 6, 7?")
+        tool_response = agent.handle_tool_call(response)
+        if tool_response is not None:
+            response = agent.receive(tool_response)
+            print(response)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    tester = TestOpenAIAPI()
+    tester.setup_method(name="", generate_data=False, generate_gold=False, debug=True)
+    tester.test_openai_api()
