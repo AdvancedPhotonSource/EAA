4a6ea68e01302683d5a8b92acaef7a6fa277ec49
Ming Du <ming.du101@gmail.com>
2025-06-29 11:18:37 -0500

    REFACTOR: encapsulate wait for file routines into a util function

--- src/eaa/util.py
+++ src/eaa/util.py
@@ -1,13 +1,19 @@
-from typing import Tuple, Literal
+from typing import Tuple, Literal, Optional
 import datetime
 import base64
 import io
 import re
 from io import BytesIO
+import os
+import time
+import logging
+from math import inf
 
 from PIL import Image
 import numpy as np
 import torch
+
+logger = logging.getLogger(__name__)
 
 
 def get_timestamp():
@@ -163,4 +169,54 @@ def get_image_path_from_text(
         return path, text
     else:
         return path
+
+
+def wait_for_file(
+    file_path: str,
+    duration: int = 30,
+    timeout: Optional[int] = inf
+) -> bool:
+    """Wait for a file to exist and stay unchanged for a given timeout.
+    The function only returns when the given file 
+    (1) exists, and
+    (2) its modification time does not change for `duration` seconds.
     
+    If `timeout` is given, the function returns False if the above conditions
+    are not met within `timeout` seconds.
+    
+    Parameters
+    ----------
+    file_path : str
+        The path to the file to wait for.
+    duration : int, optional
+        The duration that the file is expected to stay unchanged to be
+        considered as fully written.
+    timeout : int, optional
+        The timeout to wait for the file to exist and stay unchanged,
+        given in seconds.
+        
+    Returns
+    -------
+    bool
+        True if the file exists and stays unchanged for given a duration 
+        within the given timeout, 
+        False otherwise.
+    """
+    time_diff = 0
+    time_mod = 0
+    while any([time_diff < duration, not os.path.exists(file_path)]):
+        if timeout is not None:
+            if time_diff > timeout:
+                return False
+        time.sleep(1)
+        if os.path.exists(file_path):
+            if os.path.getmtime(file_path) != time_mod:
+                time_mod = os.path.getmtime(file_path)
+            time_diff = time.time() - time_mod
+            logger.info(f"File {file_path} exists.")
+            logger.info(f"Watching file and wait until the file doesn't change for {duration} seconds to process.")
+        else:
+            logger.info(f"File {file_path} does not exist.")
+            logger.info(f"Waiting for {duration} seconds to process.")
+            time.sleep(duration)
+    return True
